4fd3bf2acfdb5ed51658cb4ec017d399e0d6c478
Adanxari <adanxari6@gmail.com>
2022-02-14 21:06:38 +0500

    search update

--- shop/views.py
+++ shop/views.py
@@ -64,8 +64,32 @@ def tracker(request):
     return render(request, 'shop/tracker.html')
 
 
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if (query in item.Desctription.lower() or query in item.Product_name.lower() or query in item.category.lower()):
+        return True
+    else:
+        return False
+
+
 def search(request):
-    return render(request, 'shop/search.html')
+    query = request.GET.get('search')
+    allProds = []
+    catprods = product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    print(1, allProds)
+    if len(allProds) == 0 or len(query) < 4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 
 
 def productView(request, myid):
